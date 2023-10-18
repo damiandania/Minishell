1a99ea0faf521d19493b1e955c85fb0856be34a6
Damian Dania <damiandania@gmail.com>
2023-10-18 16:40:23 +0200

    ok

--- sources/lexer_error.c
+++ sources/lexer_error.c
@@ -5,16 +5,16 @@ bool	ft_check_sep_lexer(t_token *token)
 	if (token->type == PIPE && (token->next->type == END
 		|| token->next->type == PIPE || token->next->type != WORD))
 		return (true);
-	if (token->type == GREAT && (token->next->type == END
+	else if (token->type == GREAT && (token->next->type == END
 		|| token->next->type == PIPE || token->next->type != WORD))
 		return (true);
-	if (token->type == LESS && (token->next->type == END
+	else if (token->type == LESS && (token->next->type == END
 		|| token->next->type == PIPE || token->next->type != WORD))
 		return (true);
-	if (token->type == GREAT_GREAT && (token->next->type == END
+	else if (token->type == GREAT_GREAT && (token->next->type == END
 		|| token->next->type == PIPE || token->next->type != WORD))
 		return (true);
-	if (token->type == LESS_LESS && (token->next->type == END
+	else if (token->type == LESS_LESS && (token->next->type == END
 		|| token->next->type == PIPE || token->next->type != WORD))
 		return (true);
 	return (false);
@@ -27,14 +27,14 @@ void	ft_lexer_error(t_token *token)
 	temp = token;
 	if (temp->type == PIPE)
 	{
-			ft_putstr_fd("error: syntax token\n", 2);
+			ft_putstr_fd("syntax error near unexpected token\n", 2);
 			return ;
 	}
 	while (temp)
 	{
 		if (ft_check_sep_lexer(temp) == true)
 		{
-			ft_putstr_fd("error: syntax token\n", 2);
+			ft_putstr_fd("syntax error near unexpected token\n", 2);
 			return ;
 		}
 		temp = temp->next;
